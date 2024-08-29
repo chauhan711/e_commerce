6c48add3d747fe393948fbbb7b048b212a573adc
parveen kumar <parveen@csgroupchd.com>
2024-08-29 19:49:24 +0530

    update code

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const db = require('./db.js');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const socketIO = require('socket.io');
+const http = require('http');
+let server = http.createServer(app);
+var io = socketIO(server);
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const cors = require('cors');
@@ -14,7 +18,13 @@ app.use('/server/admin',require('./routes/admin.route.js'));
 app.use('/server/saler',require('./routes/saler.route.js'));
 app.use('/server/user',require('./routes/user.route.js'));
 
+// make connection with user from server side
+// make a connection with the user from server side
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+});
+
 // Start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
